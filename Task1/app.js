1bddcf1c487aedb60c0e4691affb3e4cf43cc564
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-11 18:47:17 +0400

    mybirthdayDate

--- Task1/app.js
+++ Task1/app.js
@@ -3,3 +3,7 @@ let rectangleWidth = "10";
 let rectangleLenght = "15";
 alert(rectangleWidth * rectangleLenght);
 
+let myBirthdayDate = "1996"
+let currentDate = "2023"
+alert(currentDate - myBirthdayDate)
+
