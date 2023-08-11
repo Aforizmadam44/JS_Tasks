d58cd594813e96ad0b72d327d1b0b797882a6169
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-11 23:13:56 +0400

    little changes 1st section

--- Task1/app.js
+++ Task1/app.js
@@ -1,11 +1,10 @@
 
 let rectangleWidth = "10";
 let rectangleLenght = "15";
-alert(rectangleWidth * rectangleLenght);
+alert("rectangleArea:" + rectangleLenght * rectangleWidth);
 
 let myBirthdayDate = "1996"
 let currentDate = "2023"
-alert(currentDate - myBirthdayDate)
 
 
 let value = "km";
