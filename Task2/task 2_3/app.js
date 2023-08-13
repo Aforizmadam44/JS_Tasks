11f64d662a9110de8cba663d4db112a84ac0099b
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-13 17:50:33 +0400

    task 2 2_3 added

---
+++ Task2/task 2_3/app.js
@@ -0,0 +1,10 @@
+let eded1 = prompt("1-ci ədədi daxil edin")
+let eded2 = prompt("2-ci ədədi daxil edin")
+let toplama =parseInt(eded1) + parseInt(eded2)
+let cixma =parseInt(eded1) - parseInt(eded2)
+let vurma =parseInt(eded1) *parseInt(eded2)
+let bolme =parseInt(eded1) / parseInt(eded2)
+console.log(toplama);
+console.log(cixma);
+console.log(vurma);
+console.log(bolme);
