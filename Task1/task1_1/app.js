fcc3c36f900550b64069f5e304c9ae425d928ddd
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-13 12:02:08 +0400

    js task1 finish

---
+++ Task1/task1_1/app.js
@@ -0,0 +1,4 @@
+let rectangleWidth= prompt("Write rectangle width")
+let rectangleHeight= prompt("Write rectangle height")
+let rectangleAreas = rectangleWidth * rectangleHeight
+alert("rectangleAreas:" + rectangleAreas)
---
+++ Task1/task1_2/app.js
@@ -0,0 +1,4 @@
+let userBirthday = prompt("Write your birthday date")
+let currentYear = prompt("Write current year")
+let userAge = currentYear - userBirthday 
+alert("userAge:" + userAge)
