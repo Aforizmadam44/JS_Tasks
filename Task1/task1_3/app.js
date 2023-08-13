4349165bba189e38d487d7e42acdb5a524a83e90
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-13 16:17:52 +0400

    task2 2_1 added

--- Task1/task1_3/app.js
+++ Task1/task1_3/app.js
@@ -1,3 +1,4 @@
 let userKm = prompt("Write km value, we changed to mile")
 let userMile = userKm * 0.6214
 alert("userMile" + userMile)
+al
---
+++ Task2/task 2_1/app.js
@@ -0,0 +1,4 @@
+let name=prompt("Adı daxil edin")
+let surname=prompt("Soyadı daxil edin")
+let age=prompt("Yaşı daxil edin")
+console.log(`I'm ${name}. My surname is ${surname}.I'm ${age}}years old.` );
