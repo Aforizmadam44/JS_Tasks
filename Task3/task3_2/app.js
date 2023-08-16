9d7c3e08478268a748c84abed5a58459a5a1895a
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-17 00:19:50 +0400

    task 3_2 added

---
+++ Task3/task3_2/app.js
@@ -0,0 +1,18 @@
+let selectedMusic = prompt(`Write your loved music :  1. Thurisaz- Endless
+2.Dang Show- Khata kardam
+3.Sagopa Kajmer- Onca şeyin ardından
+`)
+ if(selectedMusic===`1` )
+{console.log(`I was drowning in a dying moment
+Drowning in her eyes
+Feeling the wings of faith, embracing me
+So unreal but I could feel`);}
+else if(selectedMusic===`2`)
+{console.log(`
+تو در فتنه از حد به در کردی
+تو با من ز بد هم بدتر کردی
+ز سوی نگاه تب آلودت
+من تشنه را تشنه تر کردی`);}
+else if(selectedMusic===`3`){console.log(`Gizli sokaklarına daldım ömrümün
+Şahidi oldu iki gözümse gördüğümün 
+Çokları var farkında değil öldüğünün`);}
