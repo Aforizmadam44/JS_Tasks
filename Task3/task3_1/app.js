7b95af11383a57cbf0424b9ac94890de3d3b9854
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-17 00:18:28 +0400

    task 3_1 added

---
+++ Task3/task3_1/app.js
@@ -0,0 +1,12 @@
+let userName= prompt("Write your name: ")
+let userLastname= prompt("Write your Lastname: ")
+let userId= userName + userLastname
+console.log( "userId:" + userId);
+// ----------------------------------------------
+let writeName= prompt('Please write your name: ')
+console.log("~~" + writeName +"~~");
+// ----------------------------------------------
+let rectangleSideA = +(prompt('Write rectangle sideA:'))
+let rectangleSideB = +(prompt('Write rectangle sideB:'))
+let rectanglePerimeter = (rectangleSideA + rectangleSideB) *2
+console.log(rectanglePerimeter);
