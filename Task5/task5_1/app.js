2fdc82572cea76c5bdd094e4b78beb484a1d3bfb
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-25 21:24:11 +0400

    taks5_1

---
+++ Task5/task5_1/app.js
@@ -0,0 +1,10 @@
+
+// function add(eded1, eded2, callback){
+//    callback(eded1+eded2)
+//     }
+// add(3, 5, console.log)
+// add(3, 5, alert)
+// --------------------------------------
+
+ 
+//  --------------------------------------
