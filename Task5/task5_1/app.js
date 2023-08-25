7f5c6757675fbed7d9d7140cc9c2c619672f997a
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-25 22:01:51 +0400

    task5_1 new edited

--- Task5/task5_1/app.js
+++ Task5/task5_1/app.js
@@ -1,10 +1,19 @@
 
-// function add(eded1, eded2, callback){
-//    callback(eded1+eded2)
-//     }
-// add(3, 5, console.log)
-// add(3, 5, alert)
+function add(eded1, eded2, callback){
+   callback(eded1+eded2)
+    }
+add(3, 5, console.log)
+add(3, 5, alert)
 // --------------------------------------
 
- 
-//  --------------------------------------
+ function formal(cins, ad){
+    if(cins==="K"){
+        console.log(`Salam cenab ${ad}`);
+    }else if(cins==="Q"){
+        console.log(`Salam xanim ${ad}`);
+    }
+
+ }
+ formal("K", "Ismet")
+ formal("Q", "Nezrin")
+    //  --------------------------------------
