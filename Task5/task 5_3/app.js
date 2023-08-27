b069fa41df23e798a1f6867bf9f00acccda413d0
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-08-27 18:04:58 +0400

    task not completed yet

---
+++ Task5/task 5_3/app.js
@@ -0,0 +1,74 @@
+
+// function myFunction(number){
+//     if(number%2===0){
+//         console.log("Eded cutdur");
+//         return true;
+//     }else if(number%2===1){
+//         console.log("Eded tekdir");
+//         return false;
+//     } 
+// }
+// myFunction(3)
+// ------------------------------------------
+// function myNumber(number1, number2, number3){
+// Math.max(number1, number2, number3)
+// }
+// console.log(` The biggest number is ${Math.max(4, 6, 9)}`)
+// ----------------------------------------------------------------
+// task3-empty
+// -----------------------------------------------------------------------------------------
+// task4-empty
+// ----------------------------------------------------------------------------------------
+// function myNumber(number1, number2){
+//     Math.min(number1, number2)
+// }
+// console.log(`The smallest number is ${Math.min(4, 7)}`);
+// ------------------------------------------------------------------------------------------------
+// function calc   (number1, number2, mathOperators){
+//     switch (mathOperators) {
+//         case "+":
+//             return number1 + number2;
+//         case "-":
+//             return number1 - number2;
+//         case "*":
+//             return number1 * number2;
+//         case "/":
+//             return number1 / number2;
+//         default:
+//             return 0;
+//     }
+// }
+
+// console.log(calc(2, 3, "+"));
+// console.log(calc(2, 3, "-"));
+// console.log(calc(2, 3, "*"));
+// console.log(calc(2, 3, "/"));
+// ------------------------------------------------------------------------------------------------
+// function isLucky (number1, number2, number3, number4, number5, number6){
+//     if(number1+number2+number3===number4+number5+number6){
+//         return true 
+//     }else{
+//         return false
+//     }
+
+// }
+// console.log(isLucky(2,3,6,1,7,3));
+// console.log(isLucky(4,5,7,1,3,4));
+// ------------------------------------------------------------------------------------------------
+// task08- empty
+// ------------------------------------------------------------------------------------------------
+// function isEven(number1){
+   
+//     if(number1%2===0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// console.log(isEven(12));
+// console.log(isEven(55));
+// ------------------------------------------------------------------------------------------------
+// Task10-empty
+// ------------------------------------------------------------------------------------------------
+// Task11-empty
