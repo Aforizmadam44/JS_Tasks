473acd3cb34e31327c47bf8e8f815ccf5c0a39c1
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-09-13 21:54:59 +0400

    task 9 all section completed

--- Task8/app.js
+++ Task8/app.js
@@ -1,42 +1,57 @@
 // 1. Massiv yaradin.  olcusunu 5e menimsedin ve prompt-la massivi number-lerle doldurun.
 
-let array = [];
-array.length = 5;
-for (let i = 0; i < array.length; i++) [console.log(prompt("Write 5 numbers"))];
+// let array = [];
+
+// for (let i = 0; i < 5; i++) {
+//   let numbUser = +prompt("enter num");
+//   array.push(numbUser);
+// }
+
+// console.log(array);
 
 // --------------------------------------------------------------------------------------
 // 2. Funksiya yazin ki, massivde olan butun elementlerin hasilini ekrana cixarsin.
 
-const sumNumbers = (numbers) => {
-  let mult = 1;
-  for (let i = 0; i < numbers.length; i++) {
-    mult *= numbers[i];
-  }
-  return mult;
-};
-console.log(sumNumbers([1, 2, 3, 4, 5]));
+// const multNumbers = (numbers) => {
+//   let mult = 1;
+//   for (let i = 0; i < numbers.length; i++) {
+//     mult *= numbers[i];
+//   }
+//   return mult;
+// };
+// console.log(multNumbers([1, 2, 3, 4, 5]));
 
 // --------------------------------------------------------------------------------------
 // 3. multiply(a,b,c) funksiyasi yaradin. Men ona istediyim sayda argument versem
 //  bele islemelidir, hasilini geri qaytarmalidir
 
-function multiply() {
-  if (arguments.length < 2) {
-    console.log("Enter minimum 2 arguments");
-    return;
-  }
+// if (arguments.length < 2) {
+//   console.log("Enter minimum 2 arguments");
+//   return;
+// }
 
-  let mult = 1;
-  for (let i = 0; i < arguments.length; i++) {
-    mult *= arguments[i];
-  }
+//   function multiply() {
+//   let mult = 1;
+//   for (let i = 0; i < arguments.length; i++) {
+//     mult *= arguments[i];
+//   }
 
-  console.log("Mult is", mult);
-}
+//   console.log("Mult is", mult);
+// }
 
-multiply(1);
-multiply(2, 3);
-multiply(2, 5, 9);
-multiply(5, 6, 2, 3);
+// multiply(1);
+// multiply(2, 3);
+// multiply(2, 5, 9);
+// multiply(5, 6, 2, 3);
+
+// const multiply = (...arr) => {
+//   let mult = 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     mult *= arr[i];
+//   }
+//   console.log(mult);
+// };
+// multiply(2, 3, 7);
+// multiply(4, 7, 8, 9);
 
 // --------------------------------------------------------------------------------------
---
+++ Task9/app.js
@@ -0,0 +1,349 @@
+let arr = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Elvis",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "Roscoeview",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Christy",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Howemouth",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Lebsackbury",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+];
+// Task1. Get all users whose zip code is less than or equal to 50000.
+
+let zipCode = arr.map((element) => element.address.zipcode.slice(0, 5));
+let newZipeArray = [];
+let zipFilter = zipCode.filter((element) => {
+  if (element <= "50000") {
+    newZipeArray.push(element);
+  }
+});
+console.log(newZipeArray);
+// -----------------------------------------------------------------------
+// Task2. Get all users whose email ends with '.net'.
+
+let email = arr.find((element) => element.email.endsWith(".net"));
+console.log(email);
+// -----------------------------------------------------------------------
+// Task3. Get an array of objects containing only the name and email of
+// users whose name starts with 'C'.
+
+let findForC = arr.find((element) => {
+  if (element.name.startsWith("C")) {
+    console.log(`Name is :${element.name}  \n Email is :${element.email}`);
+  }
+});
+// -----------------------------------------------------------------------
+// Task4. Get an array of all user names in uppercase.
+
+let userName2 = arr.map((element) => element.name.toUpperCase());
+console.log(userName2);
+// -----------------------------------------------------------------------
+// Task5. Get the total number of users in the array.
+
+let count2 = 0;
+let userCount = arr.map((element) => {
+  if (element.username) {
+    count2 += 1;
+  }
+});
+console.log(count2);
+// -----------------------------------------------------------------------
+// Task6. Get the sum of all user IDs.
+
+let count1 = 0;
+let idCount = arr.map((element) => {
+  for (let i = 0; i < element.id; i++) {
+    count1 += 1;
+  }
+});
+console.log(count1);
+// -----------------------------------------------------------------------
+// Task7. Get the user with the highest ID.
+
+let higgestId = arr.map((element) => element.id);
+let sortId = higgestId.sort((a, b) => b - a);
+console.log(sortId[0]);
+// -----------------------------------------------------------------------
+// Task8. Get an array of all user names sorted in alphabetical order.
+
+let usernames = arr.map((element) => element.username);
+let alphabeticalSort = usernames.sort((a, b) => a.localeCompare(b));
+console.log(alphabeticalSort);
+// -----------------------------------------------------------------------
+// Task9. Get an array of users whose name starts with the letter 'P'.
+
+let findForP = arr.find((element) => {
+  if (element.name.startsWith("P")) {
+    console.log(`Name is : ${element.name}`);
+  }
+});
+// -----------------------------------------------------------------------
+// Task10. Get an array of users whose email contains the string 'org'.
+
+let findEmail = arr.find((element) => element.email.includes("org"));
+console.log(findEmail);
+// -----------------------------------------------------------------------
+// Task11. Get an array of users whose phone number starts with the area code '1-770'.
+
+let findForAreaCode = arr.find((element) => element.phone.startsWith("1-770"));
+console.log(findForAreaCode);
+// -----------------------------------------------------------------------
+// Task12. Get an array of all user addresses as strings.
+
+let userAdresses = arr.map((element) => element.address);
+console.log(userAdresses);
+// -----------------------------------------------------------------------
+// Task13. Get an array of all user cities.
+
+let userCities = arr.map((element) => element.address.city);
+console.log(userCities);
+// -----------------------------------------------------------------------
+// Task14. Get an array of all user names in lowercase.
+
+let userName = arr.map((element) => element.name.toLowerCase());
+console.log(userName);
+// -----------------------------------------------------------------------
+// Task15. Get the average length of all user names.
+
+let usernameLength = arr.map((element) => element.username.length);
+let count = 0;
+let countFind = usernameLength.forEach((element) => (count += element));
+console.log(
+  `allusername length is:${count} and avarage ${count / usernameLength.length}`
+);
+
+// -----------------------------------------------------------------------
+// Task16. Get an array of all user IDs multiplied by 2.
+
+let mapId = arr.map((element) => element.id * 2);
+console.log(mapId);
+// -----------------------------------------------------------------------
+// Task17. Get the total number of characters in all user names.
+
+let lengthUsername = arr.map((element) => element.username.length);
+let count3 = 0;
+let countFind2 = lengthUsername.forEach((element) => (count3 += element));
+console.log(count3);
+// -----------------------------------------------------------------------
