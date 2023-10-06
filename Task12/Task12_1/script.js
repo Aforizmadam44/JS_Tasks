ddfef48e90688d545ec3af08e9cc18cfbf96eda0
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-10-06 20:49:26 +0400

    task 12 completed

--- Task12/Task12_1/script.js
+++ Task12/Task12_1/script.js
@@ -1,3 +1,5 @@
+// 1. ali-nin datalarini ekrana cixar (shekili, repo sayi, follower sayi, following sayi...)
+
 // const root = document.getElementById("root");
 
 // async function getData() {
@@ -12,14 +14,50 @@
 //     const avatar = data.avatar_url;
 
 //     root.innerHTML = `
-//       <h2>Id:${id} <br>Username:${username}</h2>
-//       <img src="${avatar}"></img>
-//     `;
+//         <h2>Id:${id}<br><br> Username:${username}</h2>
+//         <img src="${avatar}">
+//       `;
 //   } catch (error) {
 //     console.error("Datas are not available", error);
 //     root.innerHTML = "Datas are not available";
 //   }
 // }
+
 // getData();
 
 //----------------------------------------------------------------
+
+// 2. follower-larinin adlarini ve shekillerini ekrana cixar.
+
+// const root = document.getElementById("root");
+// async function followerData() {
+//   const url = "https://api.github.com/users/Ali-GreenHeart/followers";
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     data.forEach((element) => {
+//       root.innerHTML += `<h2> Follower name is: ${element.login}</h2> <br>
+//       <img src="${element.avatar_url}"> `;
+//     });
+//   } catch (error) {
+//     console.error("Datas are not available", error);
+//     root.innerHTML = "Datas are not available";
+//   }
+// }
+// followerData();
+// ----------------------------------------------------------------
+
+// 3. 10 eded repo adlarini ekrana cixar.
+
+// const root = document.getElementById("root");
+// async function reposName() {
+//   const url = "https://api.github.com/users/Ali-GreenHeart/repos?_limit=10";
+//   try {
+//     const response = await fetch(url);
+//     const repoNames = await response.json();
+//     repoNames.forEach((element) => {
+//       root.innerHTML += `<h2>Repos name:${element.name}</h2>`;
+//     });
+//   } catch {}
+// }
+// reposName();
